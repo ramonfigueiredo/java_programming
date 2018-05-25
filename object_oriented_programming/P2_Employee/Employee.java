369efa86d6cc1e6237f2a8cfe2652fa5cffc742f
Ramon Figueiredo Pessoa <ramon.fgrd@gmail.com>
2018-05-25 15:20:06 -0400

    Prog2_UML_Employee implementation

---
+++ object_oriented_programming/P2_Employee/Employee.java
@@ -0,0 +1,59 @@
+package P2_Employee;
+
+/**
+ * 
+ * @author Ramon Figueiredo Pessoa
+ *
+ */
+
+public class Employee {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private int salary;
+
+	public Employee(int id, String firstName, String lastName, int salary) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.salary = salary;
+	}
+
+	public int getID() {
+		return id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getName() {
+		return firstName + " " + lastName;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public int getAnnualSalary() {
+		return salary * 12;
+	}
+
+	public int raiseSalary(int percent) {
+		salary = salary + salary * percent;
+		return salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee[" + "id=" + getID() + ", name=" + getName() + ", salary=" + getSalary() + ']';
+	}
+}
