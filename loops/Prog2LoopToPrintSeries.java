a39aea3b3fc17e95c3dfb8cdc30d1efb20be4903
Ramon Figueiredo Pessoa <ramon.fgrd@gmail.com>
2018-05-26 19:42:37 -0400

    loops/Prog2LoopToPrintSeries.java

--- loops/Prog2LoopToPrintSeries.java
+++ loops/Prog2LoopToPrintSeries.java
@@ -15,17 +15,17 @@ import java.util.Scanner;
 
 public class Prog2LoopToPrintSeries {
 	public static void main(String[] args) {
-        Scanner read = new Scanner (System.in);
-        int num; 
-        int result;
-        
-        System.out.println ("Enter the number of terms in the series: ");
-        num = read.nextInt();
-        
-        for (int i=1; i<=num; i++)
-        {
-        	System.out.print((int)(Math.pow(2, i)) + " ");
-        }
-        
+		Scanner read = new Scanner (System.in);
+		int num; 
+		int result;
+
+		System.out.println ("Enter the number of terms in the series: ");
+		num = read.nextInt();
+
+		for (int i=1; i<=num; i++)
+		{
+			System.out.print((int)(Math.pow(2, i)) + " ");
+		}
+
 	}
 }
