80e82ce50f95dea199a01c422d3aa694c1ea3d1b
Ramon Figueiredo Pessoa <ramon.fgrd@gmail.com>
2023-01-16 15:46:33 -0500

    Java stream api code examples

--- java_8_new_features/JavaStreamAPITest.java
+++ java_8_new_features/JavaStreamAPITest.java
@@ -1,7 +1,11 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * Introduced in Java 8, the Stream API is used to process collections of objects.
@@ -14,10 +18,26 @@ import java.util.stream.Collectors;
  * - Make heavy use of lambda expressions
  * - ParallelStreams make it very to multi-thread operations
  *
- *  JAVA STREAM PIPELINE:
- *  Given a stream of elements (source), he source can be filtered or sorted or mapped. At the end, the stream of elements can be collected or reduced.
+ * JAVA STREAM PIPELINE:
+ * Given a stream of elements (source), he source can be filtered or sorted or mapped. At the end, the stream of elements can be collected or reduced.
  *
- * A stream pipeline consists of a source, followed by zero or more intermediate operations; and a terminal operation.
+ * Stream source: Streams can be created from Collections, List, Sets, ints, longs, doubles, arrays, lines of a file
+ *
+ * Stream operations are either intermediate or terminal
+ * - Intermediate operations such as filter, map, or sort return a stream so we can chain multiple intermediate operations.
+ * - Terminal operations such as forEach, collect or reduce are either void or return a non-stream result.
+ *
+ * Intermediate operations:
+ * - Zero or more intermediate operations are allowed.
+ * - Order matters for large datasets: filter list, then sort or map.
+ * - For very large datasets use ParallelStream to enable multiple threads.
+ * - Intermediate operations include: anyMatch(), distinct(), filter(), findFirst(), flatmap(), map(), skip(), sorted()
+ *
+ * Terminal operations: One terminal operation is allowed.
+ * - forEach applies the same function to each element.
+ * - collect saves the elements into a colletion.
+ * - other options reduce the stream to a single summary element: count(), max(), min(), reduce(), summaryStatistics()
+ *
  *
  * The features of Java stream are:
  *
@@ -27,34 +47,148 @@ import java.util.stream.Collectors;
  * Terminal operations mark the end of the stream and return the result.
  */
 
-public class JavaStreamAPI {
-    public static void main(String[] args) {
-        System.out.println("Printing 100 integers values using Java Streams: values.parallelStream().forEach(System.out::println)");
+public class JavaStreamAPITest {
+    public static void main(String[] args) throws IOException {
+        System.out.println("0) Printing 100 integers values using Java Streams: values.parallelStream().forEach(System.out::println)");
         List<Integer> values = new ArrayList<>();
         for(int i=1; i<=100; i++) {
             values.add(i);
         }
         values.parallelStream().forEach(System.out::println);
+        System.out.println();
 
-        System.out.println("\n");
-        System.out.println("Different Operations On Streams: Stream > Filter > Sort > Map Collect > Reduce");
-        System.out.println("JAVA STREAM PIPELINE: Given a stream of elements (source), he source can be filtered or sorted or mapped. At the end, the stream of elements can be collected or reduced.");
-        System.out.println("\n");
-        /*
-         * Different Operations On Streams
-         * Intermediate Operations:
-         *
-         * map: The map method is used to returns a stream consisting of the results of applying the given function to the elements of this stream.
-         * List number = Arrays.asList(2,3,4,5);
-         * List square = number.stream().map(x->x*x).collect(Collectors.toList());
-         *
-         * filter: The filter method is used to select elements as per the Predicate passed as argument.
-         * List names = Arrays.asList("Reflection","Collection","Stream");
-         * List result = names.stream().filter(s->s.startsWith("S")).collect(Collectors.toList());
-         *
-         * sorted: The sorted method is used to sort the stream.
-         * List names = Arrays.asList("Reflection","Collection","Stream");
-         * List result = names.stream().sorted().collect(Collectors.toList());
-         */
+        System.out.println("1) Integer stream");
+        IntStream
+                .range(1, 10)
+                .forEach(System.out::println);
+        System.out.println();
+
+        System.out.println("2) Integer stream with skip");
+        IntStream
+                .range(1, 10)
+                .skip(5)
+                .forEach(x -> System.out.println(x));
+        System.out.println();
+
+        System.out.println("3) Integer stream with sum");
+        System.out.println(
+                IntStream
+                        .range(1, 5)
+                        .sum()
+        );
+        System.out.println();
+
+        System.out.println("4) Stream.of, sorted and findFirst");
+        Stream.of("Canada", "Brazil", "USA", "France")
+                .sorted()
+                .findFirst()
+                .ifPresent(System.out::println);
+        System.out.println();
+
+        System.out.println("5) Stream from Array, sort, filter and print");
+        String[] countries = {"Venezuela", "Peru", "Brazil", "Ecuador", "Bolivia", "Chile",
+                "Paraguay", "Argentina", "Uruguay", "Colombia", "French Guyana", "Suriname"};
+        Arrays.stream(countries)
+                .filter(x -> x.startsWith("B"))
+                .sorted()
+                .forEach(System.out::println);
+        System.out.println();
+
+        System.out.println("6) Stream from Array, sort, filter and print");
+        Arrays.stream(new int[] {2, 4, 6, 8, 10})
+                .map(x -> x * x) // {2, 4, 6, 8, 10} -> {4, 16, 36, 64, 100}
+                .average() // sum(4, 16, 36, 64, 100) = 220 / 5 = 44
+                .ifPresent(System.out::println);
+        System.out.println();
+
+        System.out.println("7) Stream from list, filter and print");
+        List<String> countriesList = Arrays.asList("Peru", "Brazil", "Ecuador", "Bolivia", "Chile");
+        countriesList
+                .stream()
+                .map(String::toLowerCase)
+                .filter(x -> x.startsWith("b"))
+                .forEach(System.out::println);
+        System.out.println();
+
+        System.out.println("8) Stream rows from text file, sort, filter, and print");
+        String filePath = System.getProperty("user.dir") + File.separator + "src" + File.separator + "countries.txt";
+        System.out.println(filePath);
+        Stream<String> countriesInfile = Files.lines(Paths.get(filePath));
+        countriesInfile
+                .sorted()
+                .filter(x -> x.length() > 8)
+                .forEach(System.out::println);
+        countriesInfile.close();
+        System.out.println();
+
+        System.out.println("9) Stream rows from text file, sort, filter, and print");
+        filePath = System.getProperty("user.dir") + File.separator + "src" + File.separator + "countries.txt";
+        System.out.println(filePath);
+        List<String> countriesInfile2 = Files.lines(Paths.get(filePath))
+                .filter(x -> x.contains("B"))
+                .collect(Collectors.toList());
+        countriesInfile2.forEach(x -> System.out.println(x));
+        System.out.println();
+
+        System.out.println("10) Stream rows from CSV file and count");
+        String csvFilePath = System.getProperty("user.dir") + File.separator + "src" + File.separator + "data.csv";
+        System.out.println(csvFilePath);
+        Stream<String> validCSVRows = Files.lines(Paths.get(csvFilePath));
+        int rowCount = (int) validCSVRows
+                .map(x -> x.split(","))
+                .filter(x -> x.length == 3)
+                .count();
+        System.out.println(rowCount + " rows.");
+        validCSVRows.close();
+        System.out.println();
+
+        System.out.println("11) Stream rows from CSV file and parse data from row");
+        csvFilePath = System.getProperty("user.dir") + File.separator + "src" + File.separator + "data.csv";
+        System.out.println(csvFilePath);
+        Stream<String> validCSVRows2 = Files.lines(Paths.get(csvFilePath));
+        validCSVRows2
+                .map(x -> x.split(","))
+                .filter(x -> x.length == 3)
+                .filter(x -> Integer.parseInt(x[1]) > 15)
+                .forEach(x -> System.out.println(x[0] + " " + x[1] + " " + x[2]));
+        validCSVRows2.close();
+        System.out.println();
+
+        System.out.println("12) Stream rows from CSV file and store fields in HashMap");
+        csvFilePath = System.getProperty("user.dir") + File.separator + "src" + File.separator + "data.csv";
+        System.out.println(csvFilePath);
+        Stream<String> rows3 = Files.lines(Paths.get(csvFilePath));
+        Map<String, Integer> map = new HashMap<>();
+        map = rows3
+                .map(x -> x.split(","))
+                .filter(x -> x.length == 3)
+                .filter(x -> Integer.parseInt(x[1]) > 15)
+                .collect(Collectors.toMap(
+                        x -> x[0],
+                        x -> Integer.parseInt(x[1])
+                ));
+        rows3.close();
+        for(String key: map.keySet()) {
+            System.out.println(key + " " + map.get(key));
+        }
+        // Another way to print values using forEach and Lambda
+//        map.forEach((k, v) -> System.out.println(k + " " + v));
+        System.out.println();
+
+        System.out.println("13) Sum using Reduction");
+        double total = Stream.of(7.3, 1.5, 4.8)
+                .reduce(0.0, (Double a, Double b) -> a + b);
+        System.out.println("Total: " + total);
+        System.out.println();
+
+        System.out.println("14) Reduction - summary statistics. This only works for Integers.");
+        IntSummaryStatistics summaryStatistics = IntStream.of(7, 2, 19, 88, 73, 4, 10)
+                .summaryStatistics();
+        System.out.println(summaryStatistics);
+        System.out.println("Count: " + summaryStatistics.getCount());
+        System.out.println("Sum: " + summaryStatistics.getSum());
+        System.out.println("Min: " + summaryStatistics.getMin());
+        System.out.println("Average: " + summaryStatistics.getAverage());
+        System.out.println("Max: " + summaryStatistics.getMax());
     }
 }
